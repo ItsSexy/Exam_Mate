030cbd34782a214604b8f28bbf5dd58bde9c849e
elann <elann.fraite@gmail.com>
2024-01-05 18:02:33 +0100

    Filter types for PC

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,6 +2,7 @@
 MOBILE NAV
 =============================== */
 
+const bodyEl = document.body
 const hiddenNavEl = document.querySelector(".mobilenav__hidden");
 const openNavButtonEl = document.querySelector(".mobilenav__open");
 const closeNavButtonEl = document.querySelector(".mobilenav__close");
@@ -12,11 +13,13 @@ closeNavButtonEl.addEventListener("click", OnCloseNav)
 function OnOpenNav(e){
 
     hiddenNavEl.style.left = "0%";
+    bodyEl.style.overflow = "hidden";
 
 }
 
 function OnCloseNav(e){
 
     hiddenNavEl.style.left = "100%";
+    bodyEl.style.overflow = "scroll";
 
 }
