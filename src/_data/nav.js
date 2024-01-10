35ad726075048780121203cbb38086d971092021
AjayAthena <arno.joachim@etu.iad-arts.be>
2024-01-10 17:56:19 +0100

    Examen corrections

---
+++ src/_data/nav.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    navLabel: "Home",
+    navUrl: "/",
+  },
+  {
+    navLabel: "Collection",
+    navUrl: "/collection/",
+  },
+  {
+    navLabel: "About",
+    navUrl: "/about/",
+  },
+  {
+    navLabel: "Shop",
+    navUrl: "/shop/",
+  },
+];
